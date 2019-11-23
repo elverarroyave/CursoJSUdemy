67d1e1ed675ade4aff0364701f179d7bff491316
Elverarroyave <elver.arroyave@gmail.com>
2019-11-23 14:25:12 -0500

    4.01 Trabajamos las sentecia if-else

---
+++ 02-HTML5-CSS3/01-if_else.js
@@ -0,0 +1,14 @@
+/************************
+ * Sentencia if / else
+ */
+
+ 
+var nombre = 'pablo';
+var estadoCivil = 'Soltero';
+
+if(estadoCivil === 'Casado'){
+    console.log(nombre + ' si esta ' + estadoCivil);
+}else{
+    console.log(nombre + ' no esta ' + estadoCivil);
+}
+
