761926f011c8f809ace08ab5e2b54f10351342dc
Elverarroyave <elver.arroyave@gmail.com>
2019-11-23 19:17:43 -0500

    4.05 Trabajamos con switch

---
+++ 02-HTML5-CSS3/04-OperadorTernario.js
@@ -0,0 +1,10 @@
+/************
+ * Operador ternario
+  
+ */
+
+ var nombre = 'Pablo';
+ var edad = 19;
+
+ edad >= 18 ? console.log(nombre + ' es mayor de edad') : console.log(nombre + ' No es mayor de edad');
+
